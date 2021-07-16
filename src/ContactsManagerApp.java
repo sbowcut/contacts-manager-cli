da836326802e6d6bcbf2c19e17a4c612648d4e7c
Sam Bowcut <sbowcut@gmail.com>
2021-07-16 10:11:18 -0600

    framework

---
+++ src/ContactsManagerApp.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactsManagerApp {
+    public static void main(String[] args) {
+
+    }
+    public static void testContacts(){
+        List<Contact> contacts = new ArrayList<>();
+        Contact contactOne = new Contact("Sam Bowcut", "89087348328");
+        Contact contactTwo = new Contact("Erik Ayala", "48492839748");
+        Contact contactThree = new Contact("Billy Bob", "2347382993");
+        // Add contacts to list
+        // loop over contacts and print name/phonenumber to console
+    }
+}
