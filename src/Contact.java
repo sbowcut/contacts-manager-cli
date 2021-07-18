687ea5b769e418c8a89557f9e4c12ef121fcb30f
Sam Bowcut <sbowcut@gmail.com>
2021-07-18 17:37:14 -0600

    finishing functionality

--- src/Contact.java
+++ src/Contact.java
@@ -1,25 +1,45 @@
+import java.util.Scanner;
+
 public class Contact {
-    private String name;
-    private String phoneNumber;
+    private final Scanner scanner;
 
-    public Contact(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
+    public Contact() {
+        this.scanner = new Scanner(System.in);
     }
 
-    public String getName() {
-        return name;
+    public String getString() {
+        return this.scanner.nextLine();
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return this.scanner.nextLine();
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public int getInt() {
+        return this.scanner.nextInt();
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public long getInt(String prompt) {
+        long number;
+        try {
+            number = Long.parseLong(getString(prompt));
+            return number;
+        } catch (NumberFormatException nfe) {
+            System.out.println("Wrong input, try again: ");
+            return getInt(prompt);
+        }
+    }
+
+    public int getInt(String prompt, int min, int max) {
+        System.out.println(prompt);
+        int userNumber = this.scanner.nextInt();
+
+        if (userNumber >= min && userNumber <= max) {
+            return userNumber;
+        } else {
+            System.out.println("Not a valid selection, try again.");
+            return getInt();
+        }
     }
 }
